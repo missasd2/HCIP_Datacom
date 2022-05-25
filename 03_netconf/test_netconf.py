068091fc7ce9e03ae830e90d4a67497a32b00bf5
missasd2 <44044664+missasd2@users.noreply.github.com>
2022-05-26 00:10:12 +0800

    define class ssh

--- 03_netconf/test_netconf.py
+++ 03_netconf/test_netconf.py
@@ -4,3 +4,21 @@ import paramiko
 import time
 
 # 设备参数
+ip = '192.168.56.100'
+ssh_user = "python"
+ssh_password = "Huawei12#$"
+netconf_port = "830"
+netconf_user = "netconf"
+netconf_password = "Huawei12#$"
+filename = "netconf.txt"
+
+# 定义类ssh,用于配置设备netconf
+class ssh():
+    def ssh_connect(self, ip, username, password):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=ip, username=username, password=password)
+        print(ip + " login successfully")
+        return ssh
+
+
